cc8dfa8b6ec0e4b3d5d55449bb738c9d7b4c75e2
regone7 <regone12345@gmail.com>
2024-03-04 13:53:05 +0600

    search value js add

--- js/app.js
+++ js/app.js
@@ -4,8 +4,8 @@ const discusses = async () => {
     const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts');
     const data = await res.json();
     const alldata = data.posts;
+
     const discueee = document.getElementById('discuss');
-    const msgbtn = document.getElementById('massagebtn');
 
 
     alldata.forEach(item => {
@@ -43,73 +43,120 @@ const discusses = async () => {
                                         </div>
                                     </div>
                                     <div>
-                                        <button class="btn btn-circle bg-green-300 "><img  class="w-full massage" src="./images/email 1.png" alt=""></button>
+                                        <button  class="btn btn-circle bg-green-300 "><img onclick="getID('${item.category}')" class="w-full " src="./images/email 1.png" alt=""></button>
                                     </div>
                                 </div>
                             </div>`
         discueee.appendChild(div);
-
-
-
-        //title show
-        const allbtn = document.getElementsByClassName('massage');
-
-        let count = 0;
-
-       
-        
-
-        for (const btnt of allbtn) {
-            btnt.addEventListener('click', function () {
-                console.log(btnt)
-                count++;
-                document.getElementById('counted').innerText = count;
-                const div1 = document.createElement('div');
-                div1.className = `bg-white rounded-md p-3 my-3`;
-                div1.innerHTML = ` <div class="lg:flex ">
-                <h1 class="font-bold flex flex-col">${item.title}</h1>
-                <div class="flex justify-center items-center">
-                    <img src="./images/Group 16.png" alt="">
-                    <p>${item.view_count}</p>
-                </div>
-            </div>`
-                
-                msgbtn.appendChild(div1);
-                
-
-            })
-        }
-           
-        
-        
     
-
-
-
-
-
     
      });
-
-
-
-
+     
+     
 }
 
 discusses();
 
+let count = -1;
+const getID= async(catID)=>{
+    const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${catID}`);
+    const data = await res.json();
+    const alldata1 = data.posts;
+    count++;
+    document.getElementById('counted').innerText = count;
+    console.log(alldata1)
+
+    const msgbtn = document.getElementById('massagebtn');
+    
+
+    alldata1.forEach(item1 =>{
+                const div1 = document.createElement('div');
+                div1.className = `bg-white rounded-md p-3 my-3`;
+                div1.innerHTML = ` <div class="lg:flex ">
+                <h1 class="font-bold flex flex-col">${item1.title}</h1>
+                <div class="flex justify-center items-center">
+                    <img src="./images/Group 16.png" alt="">
+                    <p>${item1.view_count}</p>
+                </div>
+            </div>`
+            msgbtn.appendChild(div1);
+    })
+    
+ }
+
+ getID();
+
+// search value
+const searchValues = async(serID)=>{
+    const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${serID}`);
+    const data = await res.json();
+    const alldata = data.posts;
+
+    const discueee = document.getElementById('discuss');
+    discueee.innerHTML=''
+
+
+    alldata.forEach(item => {
+
+        const div = document.createElement('div');
+
+        div.className = `bg-amber-50 flex gap-3 rounded-md p-1`;
+        div.innerHTML = ` 
+                            <div class="${item.isActive ? 'avatar online placeholder ' : 'avatar offline placeholder'}">
+                                <div class="w-24 rounden-full">
+                                    <div class="grid w-32 h-full bg-base-300 place-items-center"><img src=${item.image}alt=""></div>
+                                </div>
+                            </div>   
+                            <div class="p-3 space-y-2">
+                                <div class="flex gap-3">
+                                    <p># <span>${item.category}</span></p>
+                                    <p>Author: <span>${item.author.name}</span></p>
+
+                                </div>
+                                <h1 class="font-bold">${item.title}</h1>
+                                <p>${item.description}</p>
+                                <div class="lg:flex justify-between items-center space-x-20 ">
+                                    <div class="lg:flex gap-3">
+                                        <div class="flex">
+                                            <img src="./images/Group 13.png" alt="">
+                                            <p>${item.comment_count}</p>
+                                        </div>
+                                        <div class="flex">
+                                            <img src="./images/Group 16.png" alt="">
+                                            <p>${item.view_count}</p>
+                                        </div>
+                                        <div class="flex">
+                                            <img src="./images/Group 18.png" alt="">
+                                            <p>${item.posted_time}</p>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <button  class="btn btn-circle bg-green-300 "><img onclick="getID('${item.category}')" class="w-full " src="./images/email 1.png" alt=""></button>
+                                    </div>
+                                </div>
+                            </div>`
+        discueee.appendChild(div);
+    
+    
+     });
+     
+     
+}
 
 // handel search button
-// const handelSearch =()=>{
-//     const searchFild=document.getElementById('searchFild');
-//     const inputSearched =searchFild.value;
-//     if(inputSearched){
-        
-//     }
-//     else{
-//         alert('please enter valid catagory')
-//     }
-// }
+
+const handelSearch =()=>{
+    const searchFild=document.getElementById('searchFild');
+    const inputSearched =searchFild.value;
+    if(inputSearched){
+        searchValues(inputSearched)
+    }
+    else{
+        alert('please enter valid catagory')
+    }
+}
+
+
 
 // latest post
 
@@ -119,7 +166,6 @@ const lastPost = async () => {
     const lastBT= document.getElementById('lastBtnt');
 
     data.forEach(item1 => {
-        console.log(item1)
         const div1 = document.createElement('div');
         div1.className = `card w-80 bg-base-100 shadow-xl space-x-3`;
         div1.innerHTML = `<figure class="px-5 pt-10">
